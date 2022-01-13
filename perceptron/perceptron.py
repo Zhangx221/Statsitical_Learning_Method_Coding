85dffc32bba358f8f8726cf795c61837dfb2e04f
zhangx221 <1456978852@qq.com>
2022-01-13 20:55:30 +0800

    first

---
+++ perceptron/perceptron.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+# @Time: 2022/1/13 18:28
+# @Author: 张 翔
+# @File: perceptron.py
+# @Software: PyCharm
+import pandas as pd
+import numpy as np
+from time import time
+"""
+#####################主机信息######################
+Python implementation: CPython
+Python version       : 3.8.8
+IPython version      : 7.22.0
+
+numpy     : 1.18.5
+pandas    : 1.2.2
+matplotlib: 3.3.3
+scipy     : 1.6.2
+sklearn   : 0.24.1
+
+Compiler    : MSC v.1916 64 bit (AMD64)
+OS          : Windows
+Release     : 10
+Machine     : AMD64
+Processor   : Intel64 Family 6 Model 158 Stepping 13, GenuineIntel
+CPU cores   : 8
+Architecture: 64bit
+
+#####################测试用例######################
+取 a = 0.001 iters = 50得到：
+模型用时：11.894742488861084
+模型准确率：81.58%
+
+a=0.1, iters=100000:
+模型用时：83.96011281013489
+模型准确率：79.20000%
+"""
+
+def data_load(train_filepath, test_filepath):
+    """
+    :param train_filepath: 训练集路径
+    :param test_filepath: 测试集路径
+    :return: 训练特征，测试集特征，训练集标签，测试集标签
+    """
+    train_data = pd.read_csv(train_filepath, header=None)
+    test_data = pd.read_csv(test_filepath, header=None)
+    Xtrain, ytrain = train_data.iloc[:, 1:], train_data.iloc[:, 0]
+    Xtest, ytest = test_data.iloc[:, 1:], test_data.iloc[:, 0]
+    ytrain = np.where(ytrain >= 5, 1, -1)
+    ytest = np.where(ytest >= 5, 1, -1)
+    Xtrain = Xtrain / 255           # 特征归一化用（Xtrain-最小值）/（最大值-最小值）
+    Xtest = Xtest / 255             # 用Xtrain.max().max()得知最大值为255，同理最小值为0
+    return Xtrain, Xtest, ytrain, ytest
+
+
+def perceptron(Xtrain, ytrain, a=0.001, iters=50):
+    """
+    :param Xtrain: 训练集特征
+    :param ytrain: 训练集标签
+    :param a: 学习率（learning rate，称为超参数）
+    :param iters: iters为迭代次数
+    :return: 返回模型学得的参数w,b， 分割超平面为wx+b=0， 有可能w=0但是b!=0，此时分割超平面不存在
+    """
+    w = np.zeros(Xtrain.shape[1])
+    b = 0
+    tot_num = Xtrain.shape[0]
+    i = 0
+    while i < iters:
+        for j in range(tot_num):  # 每个样本的特征
+            x = Xtrain.iloc[j, :]
+            y = ytrain[j]
+            if y * (w.dot(x) + b) <= 0:
+                w = w + a * y * x
+                b = b + a * y
+                i += 1
+    return w, b
+
+
+def score(w, b, Xtest, ytest):
+    """
+    :param w: 模型参数
+    :param b: 模型参数
+    :param Xtest: 测试集特征
+    :param ytest: 测试集标签
+    :return: 返回测试的正确率（正确样本数/总样本数）
+    """
+    w = w.values
+    tot_num = Xtest.shape[0]
+    error_num = 0
+    for j in range(tot_num):
+        x = Xtest.iloc[j, :]
+        y = ytest[j]
+        if y * (w.dot(x) + b) <= 0:
+            error_num += 1
+    return 1 - error_num / tot_num
+
+
+if __name__ == '__main__':
+    time0 = time()
+    Xtrain, Xtest, ytrain, ytest = data_load("../data/mnist_train.csv", "../data/mnist_test.csv")
+    w, b = perceptron(Xtrain, ytrain, a=0.0001, iters=100000)
+    score = score(w, b, Xtest, ytest)
+    time1 = time()
+    print(f"模型用时：{time1-time0}\n"
+          f"模型准确率：{score:.5%}")
---
+++ perceptron/test.py
@@ -0,0 +1,56 @@
+# coding=UTF-8
+# @Time: 2022/1/12 23:06
+# @Author: 张 翔
+# @File: test.py
+# @Software: PyCharm
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = (3, 3, 1)
+x2 = (4, 3, 1)
+x3 = (1, 1, -1)  # 一开始我把标签设置为0，发现到第二步就停止了，所以只能设为正负1才能使损失函数生效
+data = [x1, x2, x3]
+
+# 初始化w0和b
+w = np.array((0, 0))  # 模型参数
+w_list = []
+
+b = 0  # 模型参数
+b_list = []
+
+a = 1  # 学习率
+iters_nums = 0  # 迭代步数
+
+max_round = 100  # 最大迭代步数<=此项*样本数
+
+for i in range(max_round):
+
+	for ith in data:
+		if ith[2] * (ith[0] * w[0] + ith[1] * w[1] + b) <= 0:
+			w = w + a * ith[2] * np.array(ith[0:2])
+			b = b + a * ith[2]
+			iters_nums += 1
+			print(f"第{iters_nums}次迭代的点:{ith[0:2]}")
+			print(f"w:({w[0]},{w[1]}),b:{b}\n")
+			w_list.append(w)
+			b_list.append(b)
+
+for i in range(len(w_list)):
+	plt.scatter(np.array(data)[:, 0], np.array(data)[:, 1], c=np.array(data)[:, 2])
+	if w_list[i][0] == 0 and w_list[i][1] == 0:  # 分隔超平面不存在
+		k, b = 0, 0
+		plt.scatter(0, 0, label=f"第{i + 1}次迭代分割线")
+	elif w_list[i][1] == 0:  # 斜率无穷大（不存在）
+		plt.axvline(-b / w_list[i][0], label=f"第{i + 1}次迭代分割线")
+	else:
+		k = -w_list[i][0] / w_list[i][1]
+		b = -b_list[i] / w_list[i][1]
+		plt.plot((-2, 6), (k * (-2) + b, k * 6 + b), label=f"第{i + 1}次迭代分割线")
+	plt.xlim((-2, 6))
+	plt.ylim((-2, 6))
+	plt.xlabel("w1")
+	plt.ylabel("w2")
+	plt.axvline(0, linestyle=":", c="black")
+	plt.axhline(0, linestyle=":", c="black")
+	plt.legend()
+	plt.pause(0.5)
